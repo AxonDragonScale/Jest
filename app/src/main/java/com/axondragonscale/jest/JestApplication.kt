71f47b2f3ac8a5760d1a292bf1621352d4e0ff19
AxonDragonScale <r.r.n.harkhani@gmail.com>
2024-04-01 23:36:41 +0530

    Hilt Setup and Minor Cleanup

---
+++ app/src/main/java/com/axondragonscale/jest/JestApplication.kt
@@ -0,0 +1,10 @@
+package com.axondragonscale.jest
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Ronak Harkhani on 01/04/24
+ */
+@HiltAndroidApp
+class JestApplication: Application()
