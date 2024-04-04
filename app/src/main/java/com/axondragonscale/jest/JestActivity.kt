cfcf22140c61c9850514ae877594268afed06783
AxonDragonScale <r.r.n.harkhani@gmail.com>
2024-04-04 23:07:30 +0530

    Added JokeMapper

--- app/src/main/java/com/axondragonscale/jest/JestActivity.kt
+++ app/src/main/java/com/axondragonscale/jest/JestActivity.kt
@@ -7,15 +7,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.lifecycle.lifecycleScope
-import com.axondragonscale.jest.model.BlacklistFlag
-import com.axondragonscale.jest.model.Category
 import com.axondragonscale.jest.network.JokeApiClient
 import com.axondragonscale.jest.ui.JestApp
 import com.axondragonscale.jest.ui.theme.JestTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -35,7 +30,6 @@ class JestActivity : ComponentActivity() {
         }
 
         setupChucker()
-        test()
     }
 
     private fun setupChucker() {
@@ -44,13 +38,6 @@ class JestActivity : ComponentActivity() {
             if (status != PackageManager.PERMISSION_GRANTED)
                 notificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
         }
-    }
-
-    private fun test() = lifecycleScope.launch(Dispatchers.IO) {
-//        val jokes = apiClient.getJokes(amount = 2)
-//        println(jokes)
-        val joke = apiClient.getJoke(blacklistFlags = setOf(BlacklistFlag.Sexist, BlacklistFlag.NSFW).joinToString(separator = ","))
-        println(joke)
     }
 
 }
--- app/src/main/java/com/axondragonscale/jest/database/mapper/JokeMapper.kt
+++ app/src/main/java/com/axondragonscale/jest/database/mapper/JokeMapper.kt
@@ -2,20 +2,44 @@ package com.axondragonscale.jest.database.mapper
 
 import com.axondragonscale.jest.database.entity.JokeEntity
 import com.axondragonscale.jest.model.IJoke
-import com.axondragonscale.jest.model.IOnePartJoke
-import com.axondragonscale.jest.model.ITwoPartJoke
 import com.axondragonscale.jest.model.JokeType
+import com.axondragonscale.jest.model.OnePartJoke
+import com.axondragonscale.jest.model.TwoPartJoke
 
 /**
  * Created by Ronak Harkhani on 04/04/24
  */
 
 fun JokeEntity.toModel(): IJoke = when (type) {
-    JokeType.Single -> TODO()
-    JokeType.TwoPart -> TODO()
+    JokeType.Single -> OnePartJoke(
+        id = id,
+        lang = lang,
+        category = category,
+        flags = flags,
+        safe = safe,
+        type = type,
+        joke = joke!!,
+    )
+    JokeType.TwoPart -> TwoPartJoke(
+        id = id,
+        lang = lang,
+        category = category,
+        flags = flags,
+        safe = safe,
+        type = type,
+        setup = setup!!,
+        delivery = delivery!!,
+    )
 }
 
-fun IJoke.toEntity(): JokeEntity = when (this) {
-    is IOnePartJoke -> TODO()
-    is ITwoPartJoke -> TODO()
-}
+fun IJoke.toEntity(): JokeEntity = JokeEntity(
+    id = id,
+    lang = lang,
+    category = category,
+    flags = flags,
+    safe = safe,
+    type = type,
+    joke = (this as? OnePartJoke)?.joke,
+    setup = (this as? TwoPartJoke)?.setup,
+    delivery = (this as? TwoPartJoke)?.delivery,
+)
